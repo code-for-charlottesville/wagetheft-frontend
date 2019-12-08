5f13dbfa673e8bc22dc8a6167a1713026a863f90
Paul Rosen <paul@paulrosen.net>
2019-12-08 10:39:43 -0500

    Remove demo code

---
+++ components/SiteHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<header class="site-header">
+		<h1>VA Wage Theft</h1>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: "site-header",
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
