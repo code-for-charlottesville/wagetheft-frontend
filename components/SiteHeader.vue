10f95389ad4925fdcd757932d79a18a1d59eca22
Paul Rosen <paul@paulrosen.net>
2019-12-09 06:18:57 -0500

    Some infrastructure: mostly adding theming and scss.

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,18 +1,23 @@
 <template>
 	<header class="site-header">
-		<img src="~/assets/logo.png" alt="logo">
+		<img src="~/assets/img/logo.png" alt="logo">
 		<h1>VA Wage Theft</h1>
+<!--		<select-language></select-language>-->
 	</header>
 </template>
 
 <script>
+	// import SelectLanguage from './SelectLanguage'
 	export default {
 		name: "site-header",
+		// components: { SelectLanguage },
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+	@import "~/assets/css/variables.scss";
 	header {
 		display: flex;
+		background: $header-background;
 	}
 </style>
