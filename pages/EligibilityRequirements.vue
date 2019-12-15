c053a7b228cc1e6279c8142459fe5d07f3573d74
Paul Rosen <paul@paulrosen.net>
2019-12-15 04:07:44 -0500

    Some more functionality for the eligibility page.

--- pages/EligibilityRequirements.vue
+++ pages/EligibilityRequirements.vue
@@ -1,17 +1,23 @@
 <template>
 	<div class="eligibility-requirements">
-		<div class="request">
-			<div class="question hand-drawn lined thick">
-				{{ question }}
+		<div v-if="!notEligible">
+			<div class="request">
+				<div class="question hand-drawn lined thick">
+					{{ question }}
+				</div>
+			</div>
+			<div class="choices">
+				<button class="question hand-drawn lined thick" @click="answeredYes">
+					{{ $t('Yes') }}
+				</button>
+				<button class="question hand-drawn lined thick" @click="answeredNo">
+					{{ $t('No') }}
+				</button>
 			</div>
 		</div>
-		<div class="choices">
-			<button class="question hand-drawn lined thick" @click="answeredYes">
-				{{ $t('Yes') }}
-			</button>
-			<button class="question hand-drawn lined thick" @click="answeredNo">
-				{{ $t('No') }}
-			</button>
+		<div class="explanation-wrapper" v-if="notEligible">
+			<div class="explanation hand-drawn lined thick" v-html="explanation"></div>
+			<button class="start-over hand-drawn lined thick" @click="startOver">{{$t('start over')}}</button>
 		</div>
 	</div>
 </template>
@@ -23,18 +29,38 @@
 		computed: {
 			question() {
 				if (this.currentEligibilityStep())
-					return this.currentEligibilityStep()["en-question"];
+					return this.currentEligibilityStep()[this.$i18n.locale + "-question"];
+				return "";
+			},
+			explanation() {
+				if (this.currentEligibilityStep())
+					return this.currentEligibilityStep()[this.$i18n.locale + "-stop-explanation"];
 				return "";
 			},
 		},
+		data() {
+			return {
+				notEligible: false,
+			};
+		},
 		methods: {
 			...mapGetters(['currentEligibilityStep']),
-			...mapActions(['nextEligibilityStep']),
+			...mapActions(['nextEligibilityStep', 'setEligibilityStep']),
 			answeredYes() {
-				this.nextEligibilityStep();
+				if (this.currentEligibilityStep()['action-yes'] === 'next')
+					this.nextEligibilityStep();
+				else
+					this.notEligible = true;
 			},
 			answeredNo() {
-				this.nextEligibilityStep();
+				if (this.currentEligibilityStep()['action-no'] === 'next')
+					this.nextEligibilityStep();
+				else
+					this.notEligible = true;
+			},
+			startOver() {
+				this.setEligibilityStep(0);
+				this.notEligible = false;
 			},
 		},
 	};
@@ -48,9 +74,17 @@
 		display: flex;
 		justify-content: space-evenly;
 	}
+	.explanation-wrapper {
+	}
 	.choices {
 		margin: 30px;
 		display: flex;
 		justify-content: space-evenly;
+	}
+	.explanation {
+		display: inline-block;
+	}
+	.start-over {
+		display: inline-block;
 	}
 </style>
